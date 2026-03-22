9b517ef2f0c71198cad3fb67e2a34f6f5631793c
Mateo Fialoux <fialouxmateo@gmail.com>
2026-03-22 16:44:15 +0100

    ajout de la veille

--- src/Controller/VeilleController.php
+++ src/Controller/VeilleController.php
@@ -2,17 +2,22 @@
 
 namespace App\Controller;
 
+use App\Entity\VeilleTechnologique;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\Persistence\ManagerRegistry;
 
 final class VeilleController extends AbstractController
 {
     #[Route('/veille', name: 'app_veille')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
+        $veille = $doctrine->getRepository(VeilleTechnologique::class)
+            ->findBy([], ['date_publication' => 'DESC']);
+
         return $this->render('veille/index.html.twig', [
-            'controller_name' => 'VeilleController',
+            'veille' => $veille,
         ]);
     }
 }
