85bd63e846f377ba76f41e78bce18456c936f582
Mateo Fialoux <fialouxmateo@gmail.com>
2026-03-22 15:44:42 +0100

    Ajout des info formations et expériences dans parcours

--- src/Controller/ParcoursController.php
+++ src/Controller/ParcoursController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Experiences;
+use App\Entity\Formations;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +12,18 @@ use Symfony\Component\Routing\Attribute\Route;
 final class ParcoursController extends AbstractController
 {
     #[Route('/parcours', name: 'app_parcours')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
+        $experiences = $doctrine->getRepository(Experiences::class)
+            ->findBy([], ['date_debut' => 'DESC']);
+
+        $formations = $doctrine->getRepository(Formations::class)
+            ->findBy([], ['date_debut' => 'DESC']);
+
         return $this->render('parcours/index.html.twig', [
             'controller_name' => 'ParcoursController',
+            'experiences' => $experiences,
+            'formations' => $formations,
         ]);
     }
 }
